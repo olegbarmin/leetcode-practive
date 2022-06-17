125207940765141e43494acdd0af2016152668c3
oleg.barmin <olegbarmin@gmail.com>
2022-06-17 10:26:50 +0200

    leetcode 1: updated doc and refresh solution

--- src/main/java/com/barmin/arrays/TwoSum.java
+++ src/main/java/com/barmin/arrays/TwoSum.java
@@ -2,22 +2,24 @@ package com.barmin.arrays;
 
 import java.util.HashMap;
 
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers
+ * such that they add up to target.
+ *
+ * @leetcode 1
+ * @complexity O(n)
+ * @memory O(n)
+ */
 public class TwoSum {
 
     public static int[] twoSum(int[] nums, int target) {
-        if (nums.length < 2) {
-            throw new IllegalArgumentException();
-        }
-
-        final var map = new HashMap<Integer, Integer>();
+        final var map = new HashMap<Integer, Integer>(); // Remainder to index
         for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            int diff = target - num;
-            if (!map.containsKey(diff)) {
-                map.put(num, i);
-            } else {
-                return new int[] {map.get(diff), i};
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
             }
+            int remainder = target - nums[i];
+            map.put(remainder, i);
         }
 
         throw new IllegalArgumentException();
